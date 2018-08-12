dc8ffe69c4531355581a5559b86948562c852bcb
sid3github <siddharthpadwal3@gmail.com>
2018-08-12 22:42:16 +0530

    stream-buffer practice

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+//Below is nodejs core module which is fs module which we are requiring for file system.We use the file over here through stream.
+const fs = require('fs');
 
 let server = express();
 
+//To create a readable stream.
+let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8'); //=> utf8 for char encoding.
 
+//Below function will listen to an event, event name is given as data.
+myReadStream.on('data',function(info){
+    console.log(info);
+});
 
 
 
