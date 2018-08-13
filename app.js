a391e8b109052d866b9b764c776abf72194b95bc
sid3github <siddharthpadwal3@gmail.com>
2018-08-13 12:19:50 +0530

    stream-buffer practice

--- app.js
+++ app.js
@@ -7,20 +7,29 @@ const fs = require('fs');
 let server = express();
 
 //To create a readable stream.
-let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8'); //=> utf8 for char encoding.
+// let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8'); //=> utf8 for char encoding.
 //Use write stream to send the data anywhere from readstream.
-let myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+// let myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
 // //Below function will listen to an event, event name is given as data.
-myReadStream.on('data',function(info){
-    try{    
-    console.log(info);
-myWriteStream.write(info);    
-    }catch(err){
-        throw err;
-    }
+// myReadStream.on('data',function(info){
+//     try{    
+//     console.log(info);
+// myWriteStream.write(info);    
+//     }catch(err){
+//         throw err;
+//     }
+// });
+
+//We will be using .pipe(), which automatically read data from read stream and transfer it to the write stream.
+// myReadStream.pipe(myWriteStream);
+
+server.get('/',function(req,res){
+    let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    myReadStream.pipe(res);
 });
 
 server.listen(3000,function(){
+
     console.log('port running on 3000');
 });
